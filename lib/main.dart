abfc0fef2cce9c4241ab17c48336e2cca281f1a1
ArUn <4275969-arunptndpl@users.noreply.gitlab.com>
2026-09-02 14:58:06 +0530

    feat: Add internet and network state permissions in AndroidManifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:kvman/app/app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   runApp(const ProviderScope(child: KvManApp()));
 }
